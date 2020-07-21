afdfbd7909e85ed505a6f871de33c6356340fc32
Aditi B <aditib575@gmail.com>
2020-07-21 12:36:31 -0700

    finished user interface as well as the scroll view

--- scrollablecities/ContentView.swift
+++ scrollablecities/ContentView.swift
@@ -10,12 +10,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-    }
+        
+        ScrollView {
+            
+            VStack {
+                    Card(imageName: "paris", cityName: "Paris")
+                    Card(imageName: "london", cityName: "London")
+                     Card(imageName: "lucerne", cityName: "Lucerne")
+                     Card(imageName: "venice", cityName: "Venice")
+                     Card(imageName: "rome", cityName: "Rome")
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+}
+
+struct Card: View {
+    @State var imageName : String
+    @State var cityName :String
+    var body: some View {
+        ZStack {
+            Image(imageName).resizable().aspectRatio(contentMode: .fit)
+                .overlay(
+                    Rectangle().opacity(0.1)
+            )
+            Text(cityName).font(.system(size: 60, weight: .black, design: .rounded))
+                .foregroundColor(Color.white)
+        }.cornerRadius(30)
     }
 }
